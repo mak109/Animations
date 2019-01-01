fe8b7e78b2693f28c5b750b6b5a83c6a2192e592
mak109 <mayukhdas04@gmail.com>
2019-01-01 18:50:11 +0530

    MainActivity final commit

--- app/src/main/java/com/example/mayukh/animations/MainActivity.java
+++ app/src/main/java/com/example/mayukh/animations/MainActivity.java
@@ -7,18 +7,26 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView txtHelloWorld;
+    private TextView txtHelloWorld,txtHiWorld,txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txtHelloWorld = findViewById(R.id.txtHelloWorld);
+        txtHiWorld = findViewById(R.id.txtHiWorld);
         txtHelloWorld.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Log.i("TAG","Hey Hello Guys");
+                //Log.i("TAG","Hey Hello Guys");
+                txtHelloWorld.animate().alpha(0.0f).setDuration(2000);
+                txtHiWorld.animate().alpha(1.0f).setDuration(2000);
+                txt = txtHelloWorld;
+                txtHelloWorld = txtHiWorld;
+                txtHiWorld = txt;
             }
+
         });
+
 
     }
 }
